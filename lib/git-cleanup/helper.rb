ba95c7c9ed8c0bdd4488b9a70fb65df2f7efc84a
Doug Woos <doug@gamechanger.io>
2012-09-20 12:40:08 -0400

    -f doesn't prompt for deletion

--- lib/git-cleanup/helper.rb
+++ lib/git-cleanup/helper.rb
@@ -1,6 +1,16 @@
 class GitCleanup
   module Helper
+    @@prompt = true
+
+    def self.prompt=(val)
+        @@prompt = val
+    end
+
     def self.boolean(question, &block)
+      unless @@prompt:
+        yield
+        return true
+      end
       Formatador.display_line ""
       Formatador.display("[bold][blue][QUESTION][/] #{question} (y/n)[/] ")
       answer = STDIN.gets.chomp
